dac1ad6bac786a13114fe3a6db12ff54fb9bf750
BuildTools <unconfigured@null.spigotmc.org>
2020-07-08 18:26:25 -0700

    Update

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 Instagram Automation Functions
 
+This project is a work In progress
+
+Will be an OOP that will have various methods/classes in which a user can call methods to perform different actions. 
+Actions such as logging in/out, following users, searching hashtags/users, liking/commenting etc... 
+
 Author: Wil Sprouse
 WAVEX
--- bot.py
+++ bot.py
@@ -23,12 +23,21 @@ try:
 
     button_login = webdriver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button/div')
     button_login.click()
-    sleep(3)
+
+    sleep(8)
+
+    #button_dontSavePassword = webdriver.find_element_by_xpath('./section/main/div/div/div/section/div/button')
+    #button_dontSavePassword.click()
+    #sleep(8)
+
+    button_notifications = webdriver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')
+    button_notifications.click()
+    sleep(8)
 
     notnow = webdriver.find_element_by_css_selector('body > div:nth-child(13) > div > div > div > div.mt3GC > button.aOOlW.HoLwm')
     notnow.click() #comment these last 2 lines out, if you don't get a pop up asking about notifications
 
 except:
     pass
-    webdriver.close()
+    #webdriver.close()
 
