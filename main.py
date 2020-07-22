54999c952b6358a97754c1ad500169e98eee4f71
BuildTools <unconfigured@null.spigotmc.org>
2020-07-22 15:59:59 -0700

    Push via run.sh

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from bot import InstagramRobot
 insta = InstagramRobot('/Users/williamsprouse/Desktop/WAVEX/AutoInsta/chromedriver', '../config.ini')
 insta.start()
 insta.login()
-insta.search("_codehub_")
+#insta.search("_codehub_")
+insta.follow("__codehub_")
