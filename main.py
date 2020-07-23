0f1d3ad4d9c2db26bd598a085bb5dff81eff5c58
BuildTools <unconfigured@null.spigotmc.org>
2020-07-23 12:46:33 -0700

    Push via run.sh

--- main.py
+++ main.py
@@ -4,19 +4,12 @@ from time import sleep
 insta = InstagramRobot('/Users/williamsprouse/Desktop/WAVEX/AutoInsta/chromedriver', '../config.ini')
 insta.start()
 insta.login()
-insta.search("_codehub_")
-for i in range(5):
-	sleep(1)
-	print("Waiting..."+str(i))
-insta.unfollow("_codehub_")
+#insta.search("_codehub_")
+#insta.unfollow("_codehub_")
 #insta.follow("_codehub_")
 
 
 follow_list = ["_codehub_", "programmerplus"]
 
 for i in follow_list:
-	#insta.follow(i)
-	pass
-	pass
-
-
+	insta.follow(i)
