3bdbe9c5626a9ba56baf911eb162a83c084f6699
BuildTools <unconfigured@null.spigotmc.org>
2020-07-22 17:39:41 -0700

    Push via run.sh

--- bot.py
+++ bot.py
@@ -107,9 +107,9 @@ class InstagramRobot:
     def unfollow(self, username):
         """Must be following the user, and must be on the users page. Call search() method before this method."""
         #unfollow_button = self.driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[2]/div[2]/span/span[1]/button')
+        sleep(3)
         #unfollow_button = self.driver.find_elements_by_xpath("//*[contains(text(), 'Following')]")
-        #unfollow_button = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[2]/div[2]/span/span[1]/button/div/span')
-        unfollow_button = self.driver.find_element_by_css_selector('#react-root > section > main > div > header > section > div.Y2E37 > div.Igw0E.IwRSH.eGOV_._4EzTm > span > span.vBF20._1OSdk > button > div > span')
+        unfollow_button = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[2]/div[2]/span/span[1]/button/div/span')
         
         print(unfollow_button)
         unfollow_button.click()
