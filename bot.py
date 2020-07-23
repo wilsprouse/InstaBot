93ba7bba82539f7c1de2e3926181291b4377c11a
BuildTools <unconfigured@null.spigotmc.org>
2020-07-23 15:39:13 -0700

    Push via run.sh

--- bot.py
+++ bot.py
@@ -34,8 +34,10 @@ class InstagramRobot:
 
         sleep(1)
 
-        button_login = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button/div')
-        button_login.click()
+        #button_login = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button/div')
+        button_login = self.driver.find_elements_by_xpath('//*[contains(text(), 'Log in')]')
+        #follow_button = self.driver.find_elements_by_xpath("//*[contains(text(), 'Follow')]")
+        button_login[0].click()
 
         sleep(3)
 
