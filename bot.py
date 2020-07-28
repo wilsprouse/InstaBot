bb2f4ed87e2eeda4e122c039ebe4240b04dab475
BuildTools <unconfigured@null.spigotmc.org>
2020-07-27 18:54:38 -0700

    Push via run.sh

--- bot.py
+++ bot.py
@@ -125,11 +125,11 @@ class InstagramRobot:
 
     def comment(self, comment):
         """Picture must be open, similar to how scrollHashtag opens the pages"""
-        #comment_button = self.driver.find_element_by_class_name('_15y0l')
+        comment_button = self.driver.find_element_by_class_name('QBdPU ')
 
-        self.driver.execute_script("window.scrollTo(0, 1080)") 
+        #self.driver.execute_script("window.scrollTo(0, 1080)") 
 
-        comment_button = self.driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[3]/section[1]/span[2]')
+        #comment_button = self.driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[3]/section[1]/span[2]')
         actions = ActionChains(self.driver)
         actions.click(comment_button)
         
